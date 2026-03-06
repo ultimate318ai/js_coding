b9e9678ce1c4039e7dea4284a306a307a8484582
Nathan DAUNOIS <nathandaunois@sfr.fr>
2026-03-06 14:41:00 +0100

    fixes for data process

--- Day1/day1.js
+++ Day1/day1.js
@@ -3,25 +3,25 @@ import { readFromFile } from "../common.js";
 
 function parseFileContent(fileName) {
     const content = readFromFile(fileName);
-    const elfIndex = 0;
     return content.split('\n').reduce((callorieAmoundPerElfList, currentLine) => {
-        if (currentLine === " ") {
-            elfIndex++;
-            return callorieAmoundPerElfList;
+        if (currentLine === "") {
+            return callorieAmoundPerElfList.concat(0);
         }
-        callorieAmoundPerElfList[elfIndex] += +currentLine
+        callorieAmoundPerElfList[[callorieAmoundPerElfList.length - 1]] += +currentLine
         return callorieAmoundPerElfList
-    }, [])
+    }, [0])
 }
 
 function day1() {
-    const maxinumCaloriesIndex = parseFileContent("./input.txt").reduce((maximum, currentValue, index) => {
-        if (currentValue >= maximum) {
-            maximum = index
+    const callorieAmoundPerElfList = parseFileContent("./input.txt")
+    const { index: maxinumCaloriesIndex } = callorieAmoundPerElfList.reduce((data, currentValue, index) => {
+        if (currentValue >= data.maximum) {
+            data.index = index
+            data.maximum = currentValue
         }
-        return maximum
-    }, 0)
-    console.log(maxinumCaloriesIndex)
+        return data
+    }, { maximum: 0, index: 0 })
+    console.log(maxinumCaloriesIndex + 1) //elf's index starts at one 
 }
 
 day1()
