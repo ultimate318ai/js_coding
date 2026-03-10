660f29e3ba0fa4f1deb3177cb8c58ff86744dee0
Nathan DAUNOIS <nathandaunois@sfr.fr>
2026-03-10 15:58:14 +0100

    end of Day1

--- Day1/day1.js
+++ Day1/day1.js
@@ -11,9 +11,9 @@ function parseFileContent(fileName) {
         return callorieAmoundPerElfList
     }, [0])
 }
-
 function day1() {
     const callorieAmoundPerElfList = parseFileContent("./input.txt")
+    // first part
     const { maximum: maxinumCaloriesValue } = callorieAmoundPerElfList.reduce((data, currentValue, index) => {
         if (currentValue >= data.maximum) {
             data.index = index
@@ -22,6 +22,11 @@ function day1() {
         return data
     }, { maximum: 0, index: 0 })
     console.log(maxinumCaloriesValue)
+
+    // second part
+    const topthreeCaloriesValues = callorieAmoundPerElfList.map((value, index) => ({ value, index })).sort(({ value: a }, { value: b }) => a - b).splice(-3)
+    console.log(topthreeCaloriesValues.reduce((topthreeCaloriesValues, data) => topthreeCaloriesValues + data.value, 0))
+
 }
 
 day1()
