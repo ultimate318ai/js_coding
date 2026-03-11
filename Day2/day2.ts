9039bca58a773f213ffd92e7aa4af9e3e1a0f0c2
Nathan DAUNOIS <nathandaunois@sfr.fr>
2026-03-11 16:22:37 +0100

    add day2 first part

---
+++ Day2/day2.ts
@@ -0,0 +1,83 @@
+// --- Day 2: Rock Paper Scissors ---
+
+import { readFromFile } from "../common.js";
+
+type Round = {opponentAction: OponentAction, userAction: UserAction}
+
+type OponentAction = "A" | "B" | "C";
+
+type UserAction = "X" | "Y" | "Z";
+
+type RoundResult = "Won" | "Draw" | "Lost"
+
+const actionScore: Record<UserAction, number> = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+const roundScore: Record<RoundResult, number> = {
+    "Won": 6,
+    "Draw": 3,
+    "Lost": 0
+}
+
+
+function parseFileContent(fileName: string): {opponentAction: OponentAction, userAction: UserAction}[] {
+    const content: string = readFromFile(fileName);
+    return content.split('\n').map((currentLine) => {
+        const [opponentAction, userAction] = currentLine.split(" ") as [OponentAction, UserAction]
+        return { opponentAction, userAction }
+    })
+}
+
+function parseRound(action: Round) {
+    return actionScore[action.userAction] + roundScore[getRoundResult(action)]
+
+}
+
+function getRoundResult(action: Round): RoundResult {
+
+    const {opponentAction, userAction} = action;
+
+    switch (opponentAction) {
+        case "A":
+            switch (userAction) {
+                case "X":
+                    return "Draw"
+                case "Y":
+                    return "Won"
+                case "Z":
+                    return "Lost"
+            }
+        case "B":
+            switch (userAction) {
+                case "X":
+                    return "Lost"
+                case "Y":
+                    return "Draw"
+                case "Z":
+                    return "Won"
+            }
+        case "C":
+            switch (userAction) {
+                case "X":
+                    return "Won"
+                case "Y":
+                    return "Lost"
+                case "Z":
+                    return "Draw"
+            }
+    }
+}
+
+
+function day2() {
+
+    const resultScoreList =  parseFileContent("./input.txt").map((round) => parseRound(round))
+    const roundsResult = resultScoreList.reduce((currentScore, score) => currentScore + score, 0)
+    // console.log(resultScoreList)
+    console.log(roundsResult)
+}
+
+day2()
